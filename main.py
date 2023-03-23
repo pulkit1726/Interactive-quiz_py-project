33aa6b22eca97d7d6367681d0d4e3bb693a3971b
yashitaprajapati <122520276+yashitaprajapati@users.noreply.github.com>
2023-03-23 09:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 
+import tkinter
+from tkinter import*
+import random
+questions = [
+    "In which year, Partition of Bengal was annulled?",
+    "On which date, India's Constituent Assembly adopted the National Flag of India?",
+    "Which among the following is not a power of RBI?",
+    "Which among the following planets revolves around the sun from east to west?",
+    "Which day is celebrated as World Peace and Understanding Day?",
+    "The FIFA Football World Cup is normally held every how many years?",
+    "Which of the following languages are directly executable by a computer's CPU?",
+    "The Punjab National Bank (PNB) has signed an agreement with which telecom company to roll out open mobile wallet Speedpay?",
+    "Which city hosted the Shanghai Cooperation Organization (SCO) Film Festival 2023?",
+    "Amazon recently partnered with which leading retail Group of India, to deepen its market presence?",
+]
+
+answer_choice =[
+    ["1909","1910","1911","1912",],
+    ["26 January,1947","26 November,1947"," 22 July,1947"," 26 January,1950",],
+    ["inspection of banks","Nationalization of Banks","Call of returns","Control the lending policies of the banks",],
+    ["Venus","Mercury","Earth","Jupiter",],
+    ["23 February","29 January","10 March","17 July",],
+    ["2","3","4","5",],
+    ["Machine Language","High Language","Assembly Language","Second generation Language",],
+    ["Vodafone India","BSNL","Reliance Jio","Idea Cellular",],
+    ["Gandhinagar","Mumbai","Mysuru","Kochi",],
+    ["Future Group","D-Mart","Mahindra Retail","Big Basket",],
+]
